9794979e2c711486264e463176f9978b60c32df9
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-01-09 19:24:47 +0100

    Agregar puerto a la configuración de conexión a la base de datos

--- app/database.py
+++ app/database.py
@@ -13,6 +13,7 @@ def get_connection():
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
         database=os.getenv("DB_NAME", "clientes_db"),
+        port=int(os.getenv("DB_PORT", "3306")),
         charset="utf8mb4"
     )
 
